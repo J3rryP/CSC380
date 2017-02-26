6c28c22f7cfd8b407621a061efe13edeb1da9ff9
David Yeboah <dyeboah@oswego.edu>
2017-02-26 12:09:24 -0500

    made changes to abstract classes

--- LotInfo.java
+++ LotInfo.java
@@ -1,24 +1,39 @@
 public class LotInfo {
 
 	String description;
-	boolean Isoccupied;
+	boolean isOccupied;
 	String location;
 	boolean handicupped;
 	
 	public LotInfo(){
-		description = "";
-		Isoccupied = false;
-		location = "";
-		handicupped = false;
+		update("","",false,false);
+	}
+	
+	public LotInfo(String d, String l, boolean i , boolean h){
+		description = d;
+		location = l;
+		isOccupied = i;
+		handicupped = h;
 	}
 	
 	
 	boolean taken(){
-		return Isoccupied;
+		return isOccupied;
 	}
 
-	void update(){
+	void update(String des , String loc, boolean iso , boolean ish){
+		description = des;
+		location = loc;
+		isOccupied = iso;
+		handicupped = ish;
+	}
 	
+	void status(){
+		System.out.println("Lot info ");
+		System.out.println("Location : "+ location);
+		System.out.println("Description : "+ description);
+		System.out.println("Occupied : "+isOccupied);
+		System.out.println("Handicupped : "+handicupped);
 	}
 
 }
