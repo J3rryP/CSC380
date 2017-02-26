9761a4cdfcb591ccccc38158842c11dcfaa87e38
David Yeboah <dyeboah@oswego.edu>
2017-02-26 11:21:25 -0500

    made changes to abstract classes

--- Users.java
+++ Users.java
@@ -1,27 +1,51 @@
 public class Users {
 	String name;
 	String id;
-	String gender;
+	String sex;
 	int age;
 	
 
 
 	public Users(){
-		name = "";
-		id = "";
-		gender = "";
-		age = null;
+		this("","","",0);
 	}
 	
-	void addUsers(){
-		
+	public Users(String n , String id , String s , int a){
+		name = n;
+		id = id;
+		sex = s;
+		age = a;
+	}
+	
+	void setName(String n){
+		name = n;
+	}
+	
+	void setAge(int a){
+		age = a;
+	}
+	void setId(String n){
+		id = n;
 	}
 
-	void searchUsers(){
+
+	void setSex(String s){
+		sex = s;
+	}	
+	
+	String getName(){
+		return name;
 	}
 	
-	void deleteUsers(){
+	String getId(){
+		return id;
 	}
 	
+	int getAge(){
+		return age;
+	}
 	
+	String getSex(){
+		return sex;
+	}
 }
