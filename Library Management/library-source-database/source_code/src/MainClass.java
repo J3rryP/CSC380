3525f313318f6d9cbba7edf32ed6a920cbd6826e
David Yeboah <dyeboah@oswego.edu>
2017-04-04 12:14:52 -0400

    extract

---
+++ Library Management/library-source-database/source_code/src/MainClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jeremiah
+ */
+public class MainClass
+{
+    //change your mysql database connection here
+    public String StrUrl="jdbc:mysql://localhost/p2p_library";
+    public String StrUid="root";
+    public String StrPwd= "delta";
+
+    public static String StrUser;
+
+
+}
